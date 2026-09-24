1ea9d8fee9226fe8af05cdcb7b35220fb995db6e
joelnavarroja <joelnavarroja@users.noreply.github.com>
2026-09-24 23:27:44 +0000

    FakeWinReveal: fanfarria + jackpot sincronizados con el confeti

--- components/scenes/04-FakeWinReveal.tsx
+++ components/scenes/04-FakeWinReveal.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
 import { SceneTransition } from "../shared/SceneTransition";
 import { PrimaryButton } from "../shared/PrimaryButton";
 import { ConfettiBurst } from "../shared/ConfettiBurst";
 import { NARRATIVE_TEXTS } from "@/lib/content";
+import { playFanfare, playJackpot } from "@/lib/sound";
 
 export function FakeWinReveal({ onAdvance }: { onAdvance: () => void }) {
   const t = NARRATIVE_TEXTS.fakeWin;
+
+  useEffect(() => {
+    // Sincronizado con el confeti que dispara ConfettiBurst al montar:
+    // fanfarria clásica + jackpot superpuesto para que el "¡¡¡HAS
+    // GANADO!!!" suene tan grande como se ve.
+    playFanfare();
+    window.setTimeout(() => playJackpot(), 120);
+  }, []);
+
   return (
     <SceneTransition>
       <ConfettiBurst variant="realistic" />
